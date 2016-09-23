c98754c3cc03913d2eba6e08b5e65aa852f37a55
zong-zhen-jiang <r04922081@ntu.edu.tw>
2016-09-23 16:28:21 +0800

    Change column selection algorithm in Q1.

--- hw0/q1.py
+++ hw0/q1.py
@@ -2,18 +2,16 @@ import sys
 
 
 # Comlumn index and input file
-col = int(sys.argv[1])
+col_idx = int(sys.argv[1])
 in_file = sys.argv[2]
 
 # Parse input to a 2D matrix.
 with open(in_file) as file:
     mat = [[float(digit) for digit in line.split()] for line in file]
 
-# Transpose the matrix.
-mat_t = zip(*mat)
-
-# Sort the selected column.
-tmp = sorted(mat_t[col])
+# Extract the selected column and sort it.
+col = [row[col_idx] for row in mat]
+tmp = sorted(col)
 result = ','.join('{1}'.format(*k) for k in enumerate(tmp))
 
 # Write to output file.
